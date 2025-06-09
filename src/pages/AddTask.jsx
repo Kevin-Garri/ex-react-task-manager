2f85fe463ee194b5bedd250a7dd8e14a380a6681
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-06-09 11:39:32 +0200

    add: milestone1 task manager

---
+++ src/pages/AddTask.jsx
@@ -0,0 +1,8 @@
+export default function AddTask() {
+  return (
+    <div>
+      <h1>Aggiungi Task</h1>
+      <p>Qui ci sarà il Form</p>
+    </div>
+  )
+}
---
+++ src/pages/TaskList.jsx
@@ -0,0 +1,8 @@
+export default function TaskList() {
+  return (
+    <div>
+      <h1>List delle Task</h1>
+      <p>Qui ci sono le Task</p>
+    </div>
+  )
+}
