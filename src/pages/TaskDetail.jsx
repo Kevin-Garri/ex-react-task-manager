fb31b1d9fe7ac85743a83b237f83e35b3759d9b5
Kevin-Garri <kevin.alexander.garri29@gmail.com>
2025-06-10 14:28:17 +0200

    add: milestone8

--- src/pages/TaskDetail.jsx
+++ src/pages/TaskDetail.jsx
@@ -1,29 +1,36 @@
-import { useParams } from "react-router-dom"
-import { useContext } from "react"
-import { GlobalContext } from "../context/GlobalContext"
+import { useParams, useNavigate } from "react-router-dom";
+import { useContext } from "react";
+import { GlobalContext } from "../context/GlobalContext";
 
 export default function TaskDetail() {
   const { id } = useParams();
-  const { tasks } = useContext(GlobalContext);
+  const navigate = useNavigate();
+  const { tasks, removeTask } = useContext(GlobalContext);
   const task = tasks.find(t => t.id === parseInt(id));
-  if (!task)
-    return (
-      <h2>Task non trovata</h2>
-    )
+
+  if (!task) {
+    return <h2>Task non trovata</h2>;
+  }
+
+  const handleDelete = async () => {
+    try {
+      await removeTask(task.id);
+      alert("Task eliminata con successo!");
+      navigate("/");
+    } catch (error) {
+      console.error(error);
+      alert(error.message);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Dettaglio Task</h1>
+      <p><strong>Nome:</strong> {task.title}</p>
+      <p><strong>Descrizione:</strong> {task.description}</p>
+      <p><strong>Stato:</strong> {task.status}</p>
+      <p><strong>Data di Creazione:</strong> {new Date(task.createdAt).toLocaleDateString()}</p>
+      <button onClick={handleDelete}>Elimina Task</button>
+    </div>
+  );
 }
-
-const handleDelete = () => {
-  console.log("Elimino task:", task.id);
-
-}
-
-return (
-  <div>
-    <h1>Dettaglio Task</h1>
-    <p><strong>Nome:</strong> {task.title}</p>
-    <p><strong>Descrizione:</strong> {task.description}</p>
-    <p><strong>Stato:</strong> {Task.status}</p>
-    <p><strong>Data di Creazione:</strong>{new Date(task.createdAt).toLocaleDateString}</p>
-    <button onClick={handleDelete}>Elimina Task</button>
-  </div>
-)
